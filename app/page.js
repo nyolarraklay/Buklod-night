0ea5cedaa87c5b0b6d5aa289d167617c43fe480e
nyolarraklay <nyolosorii@gmail.com>
2024-10-25 23:46:49 +0200

    added pages

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
-import Image from "next/image";
-import Home from "../components/Home";
+import React from "react";
 
-export default function Page() {
+export default function FrontPage() {
   return (
-    <main>
-      <Home />
-    </main>
+    <div className="h-screen p-6 space-y-6">
+      <span className="text-red-500">
+        It's that time of the year again. Local of Oslo East Welcomes you to our
+        annual....
+      </span>
+      <h1 className="text-6xl flex flex-col items-start">
+        Buklod Night <span className="mt-1"> 2024</span>
+      </h1>
+      <p>Join us for a night of fun, food, and fellowship!</p>
+      <p>Stay tuned for more details.</p>
+    </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  console.log(isOpen);
+
   return (
     <div className="flex justify-between items-center p-3 bg-green-400 text-red-500 sticky top-0 z-50">
-      <h1 className="transition-all duration-300 font-bold text-xl">
+      <h1
+        className={
+          isOpen ? "hidden" : "transition-all duration-300 font-bold text-xl"
+        }
+      >
         Buklod Night 2024
       </h1>
-      <button className="flex sm:hidden transition-all duration-300">
+      <button
+        className={
+          isOpen ? "hidden" : "flex sm:hidden transition-all duration-300"
+        }
+        onClick={toggleMenu}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -20,13 +41,44 @@ export default function Header() {
           />
         </svg>
       </button>
+
+      {isOpen && (
+        <ul className="flex justify-center space-x-8 w-full">
+          <li onClick={toggleMenu}>
+            <Link href="/">Home</Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link href="/delegates">Delegates</Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link href="/events">Events</Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link href="/about">About</Link>
+          </li>
+          <li onClick={toggleMenu}>
+            <Link href="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      )}
+
       <div className="hidden sm:flex transition-all duration-300 opacity-0 sm:opacity-100">
         <ul className="flex justify-between w-80 space-x-4">
-          <li>Home</li>
-          <li>About</li>
-          <li>Events</li>
-          <li>Gallery</li>
-          <li>Contact</li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/delegates">Delegates</Link>
+          </li>
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/gallery">Gallery</Link>
+          </li>
         </ul>
       </div>
     </div>
