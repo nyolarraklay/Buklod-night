4f786ce5be5eb275b9d161459113316cac8bfb5f
nyolarraklay <nyolosorii@gmail.com>
2024-11-23 13:44:47 +0100

    remove scroll to

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useRef } from "react";
 import Headers from "../components/Header";
 import FrontPage from "@/components/FrontPage";
@@ -14,21 +15,9 @@ export default function HomePage() {
   const section4Ref = useRef(null);
   const section5Ref = useRef(null);
 
-  const scrollToSection = (section) => {
-    const refs = {
-      home: section1Ref,
-      delegates: section2Ref,
-      events: section3Ref,
-      gallery: section4Ref,
-      about: section5Ref,
-    };
-    const sectionTop = refs[section]?.current?.offsetTop || 0;
-    window.scrollTo({ top: sectionTop - 50, behavior: "smooth" });
-  };
-
   return (
     <div>
-      <Headers scrollToSection={scrollToSection} />
+      <Headers />
 
       <div className="flex flex-col justify-between">
         <div ref={section1Ref}>
